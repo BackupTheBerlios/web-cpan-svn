f29b3ffa229252661511c805442d40d9df57d5c6
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2005-05-06 18:49:10 +0000

    Latemp: branching the trunk as 0.2.x

---
+++ latemp/branches/maintenance-versions/0.2.x/installer/latemp-config.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+my $prefix = "<<<PREFIX>>>";
+
+my $opt_wml_include_path = 0;
+my $opt_wml_flags = 0;
+GetOptions(
+    "wml-include-path" => \$opt_wml_include_path,
+    "wml-flags" => \$opt_wml_flags,
+);
+
+my $wml_include_path = "$prefix/lib/wml/include/";
+if ($opt_wml_include_path)
+{
+    print "$wml_include_path\n";
+    exit;
+}
+if ($opt_wml_flags)
+{
+    my @inc_paths = 
+    (
+        $wml_include_path, 
+        ($ENV{'HOME'} . "/.latemp/lib/")
+    );
+    print join(" ", (map { "-I$_ --passoption=2,-I$_" } @inc_paths)) . "\n";
+    exit;
+}
+
