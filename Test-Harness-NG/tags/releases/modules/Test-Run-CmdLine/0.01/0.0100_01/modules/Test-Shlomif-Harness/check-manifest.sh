3546e915f6c51d966ba413f67d4baee9ba583143
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2005-11-29 21:28:52 +0000

    Deleting a wrong tag

--- Test-Harness-NG/tags/releases/modules/Test-Run-CmdLine/0.01/0.0100_01/modules/Test-Shlomif-Harness/check-manifest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cat file-list.txt | grep -v '/$' | sort | diff -u MANIFEST - |
-    grep -vP '^\+bin/prove$' | grep -vP '^\+check-manifest\.sh$' |
-    grep -vF META.yml | grep -v '^ ' | grep -vP '^(\+\+\+|\-\-\-|@@)'
