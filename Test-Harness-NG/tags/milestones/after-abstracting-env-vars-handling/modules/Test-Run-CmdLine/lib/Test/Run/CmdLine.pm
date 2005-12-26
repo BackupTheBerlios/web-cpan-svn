826b7a49f1d0d0d00257a4c99698bdf6bebd46ab
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2005-12-26 16:27:45 +0000

    Tagging that we've finished putting the env vars handling in Test-Run-CmdLine

---
+++ Test-Harness-NG/tags/milestones/after-abstracting-env-vars-handling/modules/Test-Run-CmdLine/lib/Test/Run/CmdLine.pm
@@ -0,0 +1,287 @@
+package Test::Run::CmdLine;
+
+use warnings;
+use strict;
+
+use Test::Run::Base;
+
+=head1 NAME
+
+Test::Run::CmdLine - Analyze tests from the command line using Test::Run
+
+=cut
+
+use vars (qw($VERSION));
+
+$VERSION = '0.0100_01';
+
+use vars (qw(@ISA));
+
+@ISA = (qw(Test::Run::Base));
+
+=head1 SYNOPSIS
+
+    use Test::Run::CmdLine;
+
+    my $tester = Test::Run::CmdLine->new(
+        'test_files' => ["t/one.t", "t/two.t"],
+    );
+
+    $tester->run();
+
+=cut
+
+__PACKAGE__->mk_accessors(qw(
+    driver_class
+    test_files
+    backend_params
+));
+
+sub _initialize
+{
+    my $self = shift;
+    my (%args) = @_;
+    my $driver_class = $args{'driver_class'} || $ENV{'TEST_HARNESS_DRIVER'} ||
+        "Test::Run::Obj";
+    $self->_set_driver_class($driver_class);
+    $self->test_files($args{'test_files'});
+    $self->_process_args(\%args);
+
+    return 0;
+}
+
+sub _process_args
+{
+    my ($self, $args) = @_;
+    if (exists($args->{backend_params}))
+    {
+        $self->backend_params($args->{backend_params});
+    }
+
+    return 0;
+}
+
+=head1 Interface Functions
+
+=head2 $tester = Test::Run::CmdLine->new('test_files' => \@test_files, ....);
+
+Initializes a new testing front end. C<test_files> is a named argument that
+contains the files to test.
+
+Other named arguments are:
+
+=over 4
+
+=item backend_params
+
+This is a hash of named parameters to be passed to the backend class (derived
+from L<Test::Run::Obj>.)
+
+=item driver_class
+
+This is the backend class that will be instantiated and used to perform
+the processing. Defaults to L<Test::Run::Obj>.
+
+=back 
+
+=head2 $tester->run()
+
+Actually runs the tests on the command line.
+
+TODO : Write more.
+
+=cut
+
+sub run
+{
+    my $self = shift;
+    my $driver_class = $self->driver_class();
+    eval "require $driver_class";
+
+    my $back_end_args = $self->get_backend_args();
+
+    my $driver = $driver_class->new(
+        'test_files' => $self->test_files(),
+        @$back_end_args,
+    );
+
+    return $driver->runtests();
+}
+
+sub _check_driver_class
+{
+    my $self = shift;
+    return $self->_is_class_name(@_);
+}
+
+sub _is_class_name
+{
+    my $self = shift;
+    my $class = shift;
+    return ($class =~ /^\w+(?:::\w+)*$/);
+}
+
+sub _set_driver_class
+{
+    my $self = shift;
+    my $driver_class = shift;
+    if (! $self->_check_driver_class($driver_class))
+    {
+        die "Invalid Driver Class \"$driver_class\"!";
+    }
+    $self->driver_class($driver_class);
+}
+
+=head1 Environment Variables
+
+The following environment variables (C<%ENV>) affect the behaviour of 
+Test::Run::CmdLine:
+
+=over 4
+
+=item HARNESS_COLUMNS
+
+This determines the width of the terminal (sets C<'Columns'>) in
+L<Test::Run::Obj>). If not specified, it will be determined according 
+to the C<COLUMNS> environment variable, that is normally specified by
+the terminal.
+
+=item HARNESS_DEBUG
+
+Triggers the C<'Debug'> option in Test::Run::Obj. Meaning, it will print
+debugging information about itself as it runs the tests.
+
+=item HARNESS_FILELEAK_IN_DIR
+
+This variable points to a directory that will be monitored. After each
+test file, the module will check if new files appeared in the direcotry
+and report them. 
+    
+It is advisable to give an absolute path here. If it is relative, it would
+be relative to the current working directory when C<$tester-E<gt>run()> was
+called.
+
+=item HARNESS_NOTTY
+
+Triggers the C<'NoTty'> option in Test::Run::Obj. Meaning, it causes 
+Test::Run::CmdLine not to treat STDOUT as if it were a console. In this
+case, it will not emit more frequent progress reports using carriage 
+returns (C<"\r">s).
+
+=item HARNESS_PERL
+
+Specifies the C<'Test_Interpreter'> variable of L<Test::Run::Obj>. This allows
+specifying a different Perl interprter to use besides C<$^X>.
+
+=item HARNESS_TIMER
+
+This variable triggers the C<'Timer'> option in Test::Run::Obj. What it
+does is causes the time that took for tests to run to be displayed.
+
+=item HARNESS_VERBOSE
+
+Triggers the C<'Verbose'> option in Test::Run::Obj. Meaning, it emits 
+the standard output of the test files while they are processed.
+
+=back
+
+=head1 Internal Functions
+
+=head2 my $args_array_ref = $tester->get_backend_args()
+
+Calculate and retrieve the arguments for the backend class (that inherits
+from L<Test::Run::Obj>) as a single array reference. Currently it appends
+the arguments of get_backend_env_args() to that of get_backend_init_args().
+
+=cut
+
+sub get_backend_args
+{
+    my $self = shift;
+
+    my $env_var_args = $self->get_backend_env_args();
+
+    my $init_args = $self->get_backend_init_args();
+
+    return [@$env_var_args, @$init_args,];
+}
+
+=head2 my $args_array_ref = $tester->get_backend_env_args()
+
+Calculate and return the arguments for the backend class, that originated
+from the environment (%ENV).
+
+=cut
+
+sub _get_backend_env_mapping
+{
+    my $self = shift;
+    return [
+        { 'env' => "HARNESS_FILELEAK_IN_DIR", 'arg' => "Leaked_Dir", },
+        { 'env' => "HARNESS_VERBOSE", 'arg' => "Verbose", },
+        { 'env' => "HARNESS_DEBUG", 'arg' => "Debug", },
+        { 'env' => "COLUMNS", 'arg' => "Columns", },
+        { 'env' => "HARNESS_COLUMNS", 'arg' => "Columns", },
+        { 'env' => "HARNESS_TIMER", 'arg' => "Timer", },
+        { 'env' => "HARNESS_NOTTY", 'arg' => "NoTty", },
+        { 'env' => "HARNESS_PERL", 'arg' => "Test_Interpreter", },
+        ];
+}
+
+sub get_backend_env_args
+{
+    my $self = shift;
+    my @args;
+    foreach my $spec (@{$self->_get_backend_env_mapping()})
+    {
+        my $env = $spec->{env};
+        my $arg = $spec->{arg};
+        if (exists($ENV{$env}))
+        {
+            push @args, ($arg => $ENV{$env});
+        }
+    }
+    return \@args;
+}
+
+=head2 my $args_array_ref = $tester->get_backend_init_args()
+
+Calculate and return the arguments for the backend class, that originated
+from the arguments passed to the (front-end) object from its constructor.
+
+=cut
+
+sub get_backend_init_args
+{
+    my $self = shift;
+    my @args;
+    if (defined($self->backend_params()))
+    {
+        push @args, (%{$self->backend_params()});
+    }
+    return \@args;
+}
+
+=head1 AUTHOR
+
+Shlomi Fish, C<< <shlomif@iglu.org.il> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-test-run-cmdline@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-Run-CmdLine>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2005 Shlomi Fish, all rights reserved.
+
+This program is released under the MIT X11 License.
+
+=cut
+
+1; # End of Test::Run::CmdLine
---
+++ Test-Harness-NG/tags/milestones/after-abstracting-env-vars-handling/modules/Test-Run-CmdLine/t/03-prove.t
@@ -0,0 +1,190 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Test::More tests => 17;
+use File::Spec;
+use File::Path;
+use Config;
+
+sub trap
+{
+    my $cmd = shift;
+    local (*SAVEERR, *ALTERR);
+    open ALTERR, ">", "alterr.txt";
+    open SAVEERR, ">&STDERR";
+    open STDERR, ">&ALTERR";
+    my $output = qx/$cmd/;
+    open STDERR, ">&SAVEERR";
+    close(SAVEERR);
+    close(ALTERR);
+    my $error = do { local $/; local *I; open I, "<", "alterr.txt"; <I>};
+    return wantarray() ? ($output, $error) : $output;
+}
+
+my $blib = File::Spec->catfile( File::Spec->curdir, "blib" );
+my $lib = File::Spec->catfile( $blib, "lib" );
+my $runprove = File::Spec->catfile( $blib, "script", "runprove" );
+my $sample_tests_dir = File::Spec->catfile("t", "sample-tests");
+my $test_file = File::Spec->catfile($sample_tests_dir, "one-ok.t");
+my $simple_fail_file = File::Spec->catfile($sample_tests_dir, "simple_fail.t");
+my $leaked_files_dir = File::Spec->catfile($sample_tests_dir, "leaked-files-dir");
+my $several_oks_file = File::Spec->catfile($sample_tests_dir, "several-oks.t");
+my $leaked_file = File::Spec->catfile($leaked_files_dir, "hello.txt");
+my $leak_test_file = File::Spec->catfile($sample_tests_dir, "leak-file.t");
+{
+    local %ENV = %ENV;
+    
+    local $ENV{'PERL5LIB'} = $lib.$Config{'path_sep'}.$ENV{'PERL5LIB'};
+    delete($ENV{'HARNESS_FILELEAK_IN_DIR'});
+    delete($ENV{'HARNESS_VERBOSE'});
+    delete($ENV{'HARNESS_DEBUG'});
+    delete($ENV{'HARNESS_COLUMNS'});
+    delete($ENV{'HARNESS_TIMER'});
+    delete($ENV{'HARNESS_NOTTY'});
+    delete($ENV{'HARNESS_PERL'});
+    delete($ENV{'TEST_HARNESS_DRIVER'});
+    $ENV{'COLUMNS'} = 80;
+    {
+        my $results = qx{$runprove $test_file};
+        
+        # TEST
+        ok (($results =~ m/All tests successful\./), 
+            "Good results from runprove");
+    }
+
+    {
+        mkdir($leaked_files_dir, 0777);
+        open O, ">", $leaked_file;
+        print O "This is the file hello.txt";
+        close(O);
+
+        local $ENV{'HARNESS_FILELEAK_IN_DIR'} = $leaked_files_dir;
+
+        my $results = qx{$runprove $leak_test_file $test_file};
+
+        # TEST
+        ok (($results =~ m/\nLEAKED FILES: new-file.txt\n/),
+            "Checking for files that were leaked");
+        rmtree([$leaked_files_dir], 0, 0);
+    }
+
+    {
+        local $ENV{'HARNESS_VERBOSE'} = 1;
+        my $results = qx{$runprove $test_file};
+        
+        # TEST
+        ok (($results =~ m/^ok 1/m),
+            "Testing is 'Verbose' if HARNESS_VERBOSE is 1.");
+    }
+    {
+        # This is a control experiment.
+        local $ENV{'HARNESS_VERBOSE'} = 0;
+        my $results = qx{$runprove $test_file};
+        
+        # TEST
+        ok (($results !~ m/^ok 1/m),
+            "Testing is not 'Verbose' if HARNESS_VERBOSE is 0.");
+    }
+    {
+        my $results = qx{$runprove -v $test_file};
+        
+        # TEST
+        ok (($results =~ m/^ok 1/m),
+            "Testing is 'Verbose' with the '-v' flag.");
+    }
+    {
+        local $ENV{'HARNESS_DEBUG'} = 1;
+        my $results = qx{$runprove $test_file};
+        
+        # TEST
+        ok (($results =~ m/# Running:/),
+            "Testing is 'Debug' if HARNESS_DEBUG is 1.");
+    }
+    {
+        my $results = qx{$runprove -d $test_file};
+        
+        # TEST
+        ok (($results =~ m/# Running:/),
+            "Testing is 'Debug' is the '-d' flag was specified.");
+    }
+    {
+        my $results = trap("$runprove $simple_fail_file");
+
+        # TEST
+        ok (($results =~ m/^\-{79}$/m),
+            "Testing that simple fail is formatted for 80 columns");
+    }
+    {
+        local $ENV{'COLUMNS'} = 100;
+        my $results = trap("$runprove $simple_fail_file");
+
+        # TEST
+        ok (($results =~ m/^\-{99}$/m),
+            "Testing that simple fail is formatted for 100 columns");
+    }
+    {
+        local $ENV{'HARNESS_COLUMNS'} = 100;
+        my $results = trap("$runprove $simple_fail_file");
+
+        # TEST
+        ok (($results =~ m/^\-{99}$/m),
+            "Testing that simple fail is formatted for 100 columns");
+    }
+    {
+        local %ENV = %ENV;
+        # delete ($ENV{'COLUMNS'});
+        my $results = trap("$runprove $simple_fail_file");
+
+        # TEST
+        ok (($results =~ m/^\-{79}$/m),
+            "Testing that Columns defaults to 80");
+    }
+    {
+        local $ENV{'HARNESS_TIMER'} = 1;
+        my $results = trap("$runprove $test_file $several_oks_file");
+        
+        # TEST
+        ok (($results =~ m/ok\s+\d+(?:\.\d+)?s$/m),
+            "Displays the time if HARNESS_TIMER is 1.");
+    }
+    {
+        my $results = trap("$runprove --timer $test_file $several_oks_file");
+        
+        # TEST
+        ok (($results =~ m/ok\s+\d+(?:\.\d+)?s$/m),
+            "Displays the time if --timer was set.");
+    }
+    {
+        my $results = trap("$runprove $test_file $several_oks_file");
+        
+        # TEST
+        ok (($results =~ m/ok$/m),
+            "Timer control experiment");
+    }
+    {
+        local $ENV{'HARNESS_NOTTY'} = 1;
+        my $results = trap("$runprove $test_file $several_oks_file");
+        
+        # TEST
+        ok (($results =~ m/All tests successful\./), 
+            "Good results from HARNESS_NOTTY");
+    }
+    {
+        local $ENV{'HARNESS_PERL'} = $^X;
+        my $results = trap("$runprove $test_file $several_oks_file");
+
+        # TEST
+        ok (($results =~ m/All tests successful\./),
+            "Good results from HARNESS_PERL");
+    }
+    {
+        my $results = trap("$runprove --perl $^X $test_file $several_oks_file");
+
+        # TEST
+        ok (($results =~ m/All tests successful\./),
+            "Good results with the '--perl' flag");
+    }
+}
+1;
+
