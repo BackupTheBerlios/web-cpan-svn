c10f868759619e3769af26d400dfc7fd2f84754d
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2007-06-23 14:29:31 +0000

    Tagging Test-Run-Plugin-ColorFileVerdicts as 0.0103

---
+++ Test-Harness-NG/tags/releases/modules/plugins/backend/Test-Run-Plugin-ColorFileVerdicts/0.0103/modules/Test-Shlomif-Harness/lib/Test/Run/Assert.pm
@@ -0,0 +1,64 @@
+package Test::Run::Assert;
+
+use strict;
+use warnings;
+
+require Exporter;
+use vars qw($VERSION @EXPORT @ISA);
+
+$VERSION = '0.03';
+
+@ISA = qw(Exporter);
+@EXPORT = qw(assert);
+
+=head1 NAME
+
+Test::Run::Assert - A Simple Assert Function.
+
+=head1 SYNPOSIS
+
+B<This module is only for internal use>.
+
+    use Test::Run::Assert;
+
+    assert ( EXPR , $name );
+
+=cut
+
+sub assert($;$)
+{
+    my ($condition, $name) = @_;
+
+    if (! $condition)
+    {
+        require Carp;
+        
+        my $msg = 
+            sprintf("Assert failed%s!", 
+                defined($name) ? " - '$name'" : ""
+            );
+
+        Carp::croak($msg);
+    }
+}
+
+=head1 AUTHOR
+
+Originally written by:
+
+Michael G Schwern C<< <schwern@pobox.com> >>
+
+Rewritten as MIT-X11 Licensed code by:
+
+Shlomi Fish L<http://www.shlomifish.org/>
+
+=head1 LICENSE
+
+This file is licensed under the MIT X11 License:
+
+http://www.opensource.org/licenses/mit-license.php
+
+=cut
+
+1;
+
