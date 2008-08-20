bc082c7ea5979eb7c243ab68aa05b4e8f0046418
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2008-08-20 15:00:08 +0000

    Removing an incorrect tag

--- Test-Harness-NG/tags/releases/0.01/0.0117/build/build-tags/build-ctags.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-(cd ../.. &&
-    ctags -f tags --recurse --totals \
-        --exclude='**/blib/**' --exclude='**/t/lib/**' \
-        --exclude='**/.svn/**' --exclude='*~' \
-        --languages=Perl --langmap=Perl:+.t
-)
--- Test-Harness-NG/tags/releases/0.01/0.0117/modules/plugins/backend/Test-Run-Plugin-CollectStats/t/00-load.t
+++
@@ -1,9 +0,0 @@
-#!perl -T
-
-use Test::More tests => 1;
-
-BEGIN {
-	use_ok( 'Test::Run::Plugin::CollectStats' );
-}
-
-diag( "Testing Test::Run::Plugin::CollectStats $Test::Run::Plugin::CollectStats::VERSION, Perl $], $^X" );
