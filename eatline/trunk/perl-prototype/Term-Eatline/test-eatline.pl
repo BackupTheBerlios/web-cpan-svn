50c7f9c1b35f95c824be53dfe1c29f56681e5971
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2008-08-30 15:24:53 +0000

    Added more to test-eatline.pl - does not work yet.

--- eatline/trunk/perl-prototype/Term-Eatline/test-eatline.pl
+++ eatline/trunk/perl-prototype/Term-Eatline/test-eatline.pl
@@ -6,10 +6,25 @@ use warnings;
 use lib './lib';
 
 use Term::Eatline;
+use Curses;
 
+eval {
 my $eatline = Term::Eatline->new();
 
+LINE_LOOP:
 while (my $line = $eatline->readline())
 {
-    print "You've entered:\n{{{{\n$line\n}}}}\n";
+    chomp($line);
+    if ($line eq "")
+    {
+        last LINE_LOOP;
+    }
+    $eatline->_main_win()->printw("You've entered:{%s}\n", $line);
+    $eatline->_main_win()->refresh();
+}
+};
+
+if ($@)
+{
+    die $@;
 }
