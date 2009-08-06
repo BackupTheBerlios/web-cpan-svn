065d8878d51babfd725ba60906039f6d2c5b8e58
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2009-08-06 11:23:12 +0000

    Tagging the Test-Run-Core release as 0.0120

---
+++ Test-Harness-NG/tags/releases/0.0120/modules/Test-Shlomif-Harness/lib/Test/Run/Straps/Base.pm
@@ -0,0 +1,57 @@
+package Test::Run::Straps::Base;
+
+use strict;
+use warnings;
+
+=head1 NAME
+
+Test::Run::Straps::Base - base class for some Straps-related classes.
+
+=head1 DESCRIPTION
+
+This class serves to abstract the common functionality between
+L<Test::Run::Straps> and L<Test::Run::Straps::StrapsTotalsObj>.
+
+=cut
+
+use Moose;
+
+extends('Test::Run::Base::Struct');
+
+use Test::Run::Straps::EventWrapper;
+
+use vars qw(@fields);
+
+@fields = (qw(
+    _event
+    _is_vms
+));
+
+sub _get_private_fields
+{
+    return [@fields];
+}
+
+has '_event' => (is => "rw", isa => "Maybe[Test::Run::Straps::EventWrapper]");
+
+sub _is_event_pass
+{
+    my $self = shift;
+
+    return $self->_event->is_pass();
+}
+
+=head1 LICENSE
+
+This file is licensed under the MIT X11 License:
+
+http://www.opensource.org/licenses/mit-license.php
+
+=head1 AUTHOR
+
+Shlomi Fish <shlomif@iglu.org.il>
+
+=cut
+
+1;
+
