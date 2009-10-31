a856ca9f207fa6d1e0d7f7ba97344e5f8f0d6e9b
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2009-10-31 12:53:06 +0000

    Tagging the XML-Grammar-Fortune release as 0.0109

---
+++ XML-Grammar-Fortune/tags/releases/cpan/0.0109/tags/Mandriva-Cooker-perl-crash-1/XML-Grammar-Fortune/module/lib/XML/Grammar/Fortune.pm
@@ -0,0 +1,198 @@
+package XML::Grammar::Fortune;
+
+use warnings;
+use strict;
+
+use Fatal (qw(open));
+
+use XML::LibXML;
+use XML::LibXSLT;
+
+use base 'Class::Accessor';
+
+__PACKAGE__->mk_accessors(qw(
+    _mode
+    _input
+    _output
+    _output_mode
+    ));
+
+=head1 NAME
+
+XML::Grammar::Fortune - convert the FortunesXML grammar to other formats and from plaintext.
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+TODO : Fill in.
+
+=head1 FUNCTIONS
+
+=head2 my $processor = XML::Grammar::Fortune->new({mode => $mode, input => $in, output => $out});
+
+Creates a new processor with mode $mode, and input and output files.
+
+=cut
+
+sub new
+{
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+
+    $self->_init(@_);
+
+    return $self;
+}
+
+sub _init
+{
+    my $self = shift;
+    my $args = shift;
+
+    $self->_mode($args->{mode});
+
+    $self->_input($args->{input});
+    $self->_output($args->{output});
+    $self->_output_mode($args->{output_mode} || "filename");
+
+    return 0;
+}
+
+=head2 $self->run({ %args})
+
+Runs the processor. If $mode is "validate", validates the document.
+
+%args may contain:
+
+=over 4
+
+=item * xslt_params
+
+Parameters for the XSLT stylesheet.
+
+=back
+
+=cut
+
+sub run
+{
+    my $self = shift;
+    my $args = shift;
+
+    my $xslt_params = $args->{'xslt_params'} || {};
+
+    my $output = $args->{'output'} || $self->_output();
+
+    my $mode = $self->_mode();
+
+    if ($mode eq "validate")
+    {
+        my $rngschema = XML::LibXML::RelaxNG->new(
+            location => "./extradata/fortune-xml.rng" 
+        );
+
+        my $doc = XML::LibXML->new->parse_file($self->_input());
+
+        my $code;
+        $code = $rngschema->validate($doc);
+
+        if ($code)
+        {
+            die "Invalid file!";
+        }
+
+        return $code;
+    }
+    elsif ($mode eq "convert_to_html")
+    {
+        my $parser = XML::LibXML->new();
+        my $xslt = XML::LibXSLT->new();
+
+        my $style_doc = $parser->parse_file("./extradata/fortune-xml-to-html.xslt");
+        my $stylesheet = $xslt->parse_stylesheet($style_doc);
+
+        my $source = XML::LibXML->new->parse_file($self->_input());
+
+        my $results = $stylesheet->transform($source, %$xslt_params);
+
+        if ($self->_output_mode() eq "string")
+        {
+            $$output .= $stylesheet->output_string($results);
+        }
+        else
+        {
+            open my $xhtml_out_fh, ">", $output;
+            binmode ($xhtml_out_fh, ":utf8");
+            print {$xhtml_out_fh} $stylesheet->output_string($results);
+            close($xhtml_out_fh);
+        }
+    }
+    
+    return;
+}
+=head1 FUNCTIONS
+
+=head1 AUTHOR
+
+Shlomi Fish, C<< <shlomif at iglu.org.il> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-xml-grammar-fortune at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=XML-Grammar-Fortune>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc XML::Grammar::Fortune
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=XML-Grammar-Fortune>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/XML-Grammar-Fortune>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/XML-Grammar-Fortune>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/XML-Grammar-Fortune>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Shlomi Fish, all rights reserved.
+
+This program is released under the following license: MIT X11.
+
+=cut
+
+1; # End of XML::Grammar::Fortune
