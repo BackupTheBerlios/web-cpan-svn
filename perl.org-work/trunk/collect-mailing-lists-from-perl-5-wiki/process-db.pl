c73a9e6ba085d22009643ca4d2db65a3de31bef0
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2009-11-27 17:41:45 +0000

    Add more functionality to process-db.pl - tweaked the wiki accordingly.

--- perl.org-work/trunk/collect-mailing-lists-from-perl-5-wiki/process-db.pl
+++ perl.org-work/trunk/collect-mailing-lists-from-perl-5-wiki/process-db.pl
@@ -6,6 +6,8 @@ use warnings;
 use Common;
 use YAML::XS qw(LoadFile DumpFile);
 use DBIx::Simple;
+# use autodie qw(:all);
+
 # Load the database.
 system("mysql " . Common::get_mysql_db_name() . "< lists.sql");
 
@@ -17,4 +19,28 @@ my $db = DBIx::Simple->connect(
 
 my $yaml = LoadFile(Common::get_yaml_fn());
 
+foreach my $page (keys(%$yaml))
+{
+    my $page_mls = $yaml->{$page}->{'mailing_lists'};
 
+    foreach my $ml (@$page_mls)
+    {
+        my $description = $ml->{'desc'};
+        my $items_raw = $ml->{'items'};
+
+        my $items = 
+        {
+            map 
+            { $_->{'canon_key'} => $_ } 
+            @$items_raw
+        };
+
+        my $name = $items->{'basename'}->{'value_content'};
+
+        if (!defined($name))
+        {
+            die "The name of Page '$page' and ML '$description' is undefined!";
+        }
+        # print "$name\n";
+    }
+}
