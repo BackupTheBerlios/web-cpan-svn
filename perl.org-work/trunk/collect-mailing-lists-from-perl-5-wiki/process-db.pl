a04665c2dfbfaace13537146ab573f479b5a090f
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2009-12-09 14:57:06 +0000

    Now updating some values in the database and adding the new MLs.

--- perl.org-work/trunk/collect-mailing-lists-from-perl-5-wiki/process-db.pl
+++ perl.org-work/trunk/collect-mailing-lists-from-perl-5-wiki/process-db.pl
@@ -12,17 +12,38 @@ use DBIx::Simple;
 system("mysql " . Common::get_mysql_db_name() . "< lists.sql");
 
 my $db = DBIx::Simple->connect(
-    "dbi:mysql:localhost:" . Common::get_mysql_db_name(), 
+    "dbi:mysql:database=" . Common::get_mysql_db_name() . ";host=localhost", 
     scalar(getpwuid($<)), 
     $ENV{'MYSQL_PASSWORD'},
 );
 
+if (!defined($db))
+{
+    die DBIx::Simple->error();
+}
+
+{
+    $db->query("ALTER TABLE mlists ADD COLUMN (defunct BOOLEAN)")
+        or die $db->error;
+
+    $db->query("UPDATE mlists SET defunct=TRUE")
+        or die $db->error;
+}
+
 my $yaml = LoadFile(Common::get_yaml_fn());
+
+sub get_item_blurb
+{
+    my $item = shift;
+
+    return $item->{'value_prelude'} .  " " . $item->{'value_content'};
+}
 
 foreach my $page (keys(%$yaml))
 {
     my $page_mls = $yaml->{$page}->{'mailing_lists'};
 
+    MAILING_LIST_LOOP:
     foreach my $ml (@$page_mls)
     {
         my $description = $ml->{'desc'};
@@ -41,6 +62,72 @@ foreach my $page (keys(%$yaml))
         {
             die "The name of Page '$page' and ML '$description' is undefined!";
         }
-        # print "$name\n";
+
+        my $result = $db->query("SELECT * FROM mlists WHERE name = ?", $name)
+            or die $db->error;
+
+        my $record = $result->hash();
+
+        if (!defined($record))
+        {
+            # print "New mailing list '$name'\n";
+            # New mailing list.
+            $db->query("INSERT INTO mlists (name) VALUES (?)", $name)   
+                or die $db->error;
+        }
+
+        # print "Found mailing list '$name'\n";
+        
+        $db->query("UPDATE mlists SET defunct = FALSE WHERE name = ?", $name)
+            or die $db->error;
+
+        my %new_vals;
+
+        my $subscribe = 
+            $items->{'subscribe'} || $items->{'all_in_one_ml_settings'}
+            ;
+
+        if (!defined($subscribe))
+        {
+            warn "The subscribe of Page '$page' and ML '$description' is undefined!";
+        }
+
+        $new_vals{'sub'} = get_item_blurb($subscribe);
+
+        my $unsub = 
+            $items->{'unsubscribe'} || $items->{'all_in_one_ml_settings'}
+            ;
+
+        if (!defined($unsub))
+        {
+            warn "The unsubscribe of Page '$page' and ML '$description' is undefined!";
+        }
+
+        $new_vals{'unsub'} = get_item_blurb($unsub);
+
+        my $help = $items->{'help'};
+
+        if ($help)
+        {
+            $new_vals{'help'} = get_item_blurb($help);
+        }
+
+        my $hp = $items->{'homepage'};
+
+        if ($hp)
+        {
+            $new_vals{'url'} = get_item_blurb($hp);
+        }
+
+        my @fields = keys(%new_vals);
+        my $query_string = "UPDATE mlists SET "
+            . join(", ", map { "$_ = ?" } @fields)
+            . " WHERE name = ?"
+            ;
+        $db->query(
+            $query_string, 
+            @new_vals{@fields},
+            $name
+        );
     }
 }
