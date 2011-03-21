5404e14230a92b15de10fa6e0f489540bdc8fd12
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2011-03-21 22:12:13 +0000

    Add the new Parser-MGC parser.

---
+++ XML-Grammar-Fiction/branches/convert-to-Parser-MGC/perl/modules/XML-Grammar-Fiction/TEMP/new-Parser-MGC-XML-Fiction-parser.pl
@@ -0,0 +1,209 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+package XML::Grammar::Fiction::Parser::MGC;
+
+use XML::Grammar::Fiction::Err;
+use XML::Grammar::Fiction::Struct::Tag;
+use XML::Grammar::Fiction::Event;
+
+use Moose;
+
+extends(
+    'Parser::MGC',
+    'XML::Grammar::Fiction::FromProto::Parser::XmlIterator',
+);
+
+sub _get_id_regex
+{
+    return qr{[a-zA-Z_\-]+};
+}
+
+my $id_regex = $self->_get_id_regex();
+
+sub _parse_opening_tag_attrs
+{
+    my $self = shift;
+    my $l = shift;
+
+    my @attrs;
+
+    my $id_regex = $self->_get_id_regex();
+
+    $l =~ m{^};
+
+    while ($$l =~ m{\G\s*($id_regex)="([^"]+)"\s*}cg)
+    {
+        push @attrs, { 'key' => $1, 'value' => $2, };
+    }
+
+    return \@attrs;
+}
+
+sub _parse_opening_tag
+{
+    my ($self) = @_;
+
+    my (undef, $tag_name, $attrs) = $self->expect( 
+        qr/<($id_regex)\s*((?:\s+${id_regex}="[^"]+")*)>/
+    );
+    $self->commit;
+
+    return XML::Grammar::Fiction::Struct::Tag->new(
+        name => $id,
+        is_standalone => $is_standalone,
+        line => $self->line_num(),
+        attrs => $self->_parse_opening_tag_attrs($attrs),
+    );
+}
+
+sub _parse_closing_tag
+{
+    my $self = shift;
+
+    if (my (undef, $id) = $self->expect(qr{</($id_regex)>}))
+    {
+        return XML::Grammar::Fiction::Struct::Tag->new(
+            name => $id,
+            line => $self->line_num(),
+        );
+    }
+    else
+    {
+        $self->throw_text_error(
+            'XML::Grammar::Fiction::Err::Parse::WrongClosingTagSyntax',
+            "Cannot match closing tag",
+        );
+    }
+}
+
+sub _merge_tag
+{
+    my $self = shift;
+    my $open_tag = shift;
+
+    my $new_elem = 
+        $self->_create_elem(
+            $open_tag, 
+            $self->_new_list($open_tag->detach_children()),
+        );
+
+    if (! $self->_tag_stack_is_empty())
+    {
+        $self->_add_to_top_tag($new_elem);
+        return;
+    }
+    else
+    {
+        return $new_elem;
+    }
+}
+
+sub _open_close_tag
+{
+    my ($self) = @_;
+
+    my @ret;
+
+    push @ret, $self->_parse_opening_tag();
+
+    push @ret, $self->sequence_of(
+        sub
+        {
+            return $self->any_of(
+                sub {
+                    my (undef, $text) = $self->expect(qr/([^<]+)/);
+
+                    return $self->_new_text([$text]);
+                },
+                sub {
+                    $self->_open_close_tag(),
+                },
+            );
+        },
+    );
+
+    push @ret, $self->_parse_closing_tag();
+
+    my $open = $ret[0];
+    my $close = $ret[-1];
+
+    if ($open->name() ne $close->name())
+    {
+        XML::Grammar::Fiction::Err::Parse::TagsMismatch->throw(
+            error => "Tags do not match",
+            opening_tag => $open,
+            closing_tag => $close,
+        );
+    }
+
+    return $self->_merge_tag($open);
+}
+
+sub parse
+{
+   my $self = shift;
+
+   return $self->sequence_of(
+
+   )
+   $self->sequence_of(
+      sub { $self->any_of(
+
+         sub { my ( undef, $tag, $delim ) = $self->expect( qr/([A-Z])(<+)/ );
+               $self->commit;
+               +{ $tag => $self->scope_of( undef, \&parse, ">" x length $delim ) }; },
+
+         sub { $self->substring_before( qr/[A-Z]</ ) },
+      ) },
+   );
+}
+
+package main;
+
+use Data::Dumper;
+
+if( !caller ) {
+   my $parser = PodParser->new;
+
+   while( defined( my $line = <> ) ) {
+      my $ret = eval { $parser->from_string( $line ) };
+      print $@ and next if $@;
+
+      print Dumper( $ret );
+   }
+}
+
+1;
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011 by Paul Evans. 
+
+This program is distributed under the MIT (X11) License:
+L<http://www.opensource.org/licenses/mit-license.php>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+=cut
