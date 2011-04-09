4913003c5819e9d7527ea882ff7536eebcc6d05f
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2011-04-09 13:19:40 +0000

    Tagging the XML-Grammar-Fiction release as 0.1.6

---
+++ XML-Grammar-Fiction/tags/releases/0.1.6/perl/modules/XML-Grammar-Fiction/lib/XML/Grammar/Fiction/ToDocBook.pm
@@ -0,0 +1,101 @@
+package XML::Grammar::Fiction::ToDocBook;
+
+use strict;
+use warnings;
+
+use Carp;
+
+use Moose;
+
+extends ("XML::Grammar::Fiction::RNG_Renderer");
+
+=head1 NAME
+
+XML::Grammar::Fiction::ToDocBook - module that converts the Fiction-XML to 
+DocBook 5.
+
+=head1 VERSION
+
+Version 0.1.6
+
+=cut
+
+our $VERSION = '0.1.6';
+
+=head2 new()
+
+Accepts no arguments so far. May take some time as the grammar is compiled
+at that point.
+
+=head2 meta()
+
+Internal - (to settle pod-coverage.).
+
+=cut
+
+sub _get_xslt_base_path
+{
+    my $self = shift;
+
+    return "fiction-xml-to-docbook.xslt";
+}
+
+=head2 translate_to_docbook
+
+=over 4
+
+=item * my $xhtml_source = $converter->translate_to_docbook({source => {file => $filename}, output => "string" })
+
+=item * my $xhtml_source = $converter->translate_to_docbook({source => {string_ref => \$buffer}, output => "string" })
+
+=item * my $xhtml_dom = $converter->translate_to_docbook({source => {file => $filename}, output => "dom" })
+
+=item * my $xhtml_dom = $converter->translate_to_docbook({source => {dom => $libxml_dom}, output => "dom" })
+
+=back
+
+Does the actual conversion. The C<'source'> argument points to a hash-ref with
+keys and values for the source. If C<'file'> is specified there it points to the
+filename to translate (currently the only available source). If 
+C<'string_ref'> is specified it points to a reference to a string, with the
+contents of the source XML. If C<'dom'> is specified then it points to an XML
+DOM as parsed or constructed by XML::LibXML.
+
+The C<'output'> key specifies the return value. A value of C<'string'> returns 
+the XML as a string, and a value of C<'dom'> returns the XML as an 
+L<XML::LibXML> DOM object.
+
+=cut
+
+sub translate_to_docbook
+{
+    my ($self, $args) = @_;
+
+    return $self->generic_translate($args);
+}
+
+=head1 AUTHOR
+
+Shlomi Fish, L<http://www.shlomifish.org/>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-xml-grammar-fiction at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=XML-Grammar-Fiction>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 Shlomi Fish, all rights reserved.
+
+This program is released under the following license: MIT X11.
+
+=cut
+
+1;
+
---
+++ XML-Grammar-Fiction/tags/releases/0.1.6/perl/modules/XML-Grammar-Fiction/t/proto-text-to-xml-using-custom-parser.t
@@ -0,0 +1,77 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More tests => 8;
+
+use Test::XML;
+
+use XML::LibXML;
+
+use XML::Grammar::Fiction::FromProto;
+
+use XML::Grammar::Fiction::FromProto::Parser::QnD;
+
+sub load_xml
+{
+    my $path = shift;
+
+    open my $in, "<", $path;
+    my $contents;
+    {
+        local $/;
+        $contents = <$in>
+    }
+    close($in);
+    return $contents;
+}
+
+my @tests = (qw(
+    sections-and-paras
+    sections-p-b-i
+    sections-p-b-i-comments
+    sections-a-href
+    ));
+
+# TEST:$num_texts=4
+
+my $grammar = XML::Grammar::Fiction::FromProto->new({
+        parser_class => "XML::Grammar::Fiction::FromProto::Parser::QnD",
+    });
+
+my $rngschema = XML::LibXML::RelaxNG->new(
+        location => "./extradata/fiction-xml.rng" 
+    );
+
+my $xml_parser = XML::LibXML->new();
+$xml_parser->validation(0);
+
+foreach my $fn (@tests)
+{
+    my $got_xml = $grammar->convert(
+        {
+            source =>
+            {
+                file => "t/data/proto-text/$fn.txt",
+            },
+        }
+    );
+
+    # TEST*$num_texts
+    is_xml ($got_xml, load_xml("t/data/xml/$fn.xml"),
+        "Output of the Proto Text \"$fn\""
+    );
+
+    my $dom = $xml_parser->parse_string($got_xml);
+
+    my $code;
+    $code = $rngschema->validate($dom);
+
+    # TEST*$num_texts
+    ok ((defined($code) && ($code == 0)),
+        "The validation of '$fn' succeeded.") ||
+        diag("\$@ == $@");
+}
+
+1;
