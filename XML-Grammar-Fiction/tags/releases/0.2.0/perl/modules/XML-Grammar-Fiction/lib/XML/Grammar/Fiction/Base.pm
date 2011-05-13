98d442bc3258dd7680f161835c387d32d0bdddda
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2011-05-13 13:58:04 +0000

    Tagging the XML-Grammar-Fiction release as 0.2.0

---
+++ XML-Grammar-Fiction/tags/releases/0.2.0/perl/modules/XML-Grammar-Fiction/lib/XML/Grammar/Fiction/Base.pm
@@ -0,0 +1,30 @@
+package XML::Grammar::Fiction::Base;
+
+use strict;
+use warnings;
+
+use Moose;
+
+=head1 NAME
+
+XML::Grammar::Fiction::Base - base class for XML::Grammar::Fiction
+classes.
+
+=head1 VERSION
+
+Version 0.2.0
+
+=cut
+
+our $VERSION = '0.2.0';
+
+=head1 METHODS
+
+=head2 $package->new({%args});
+
+Constructs a new package
+
+=cut
+
+1;
+
